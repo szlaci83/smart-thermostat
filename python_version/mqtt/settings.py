90cb12346fc8d5f76e4fa92f8d084c9cf7b9b47d
laszlo <laszlo@elementalconcept.com>
2018-10-16 23:48:08 +0100

    added timer

--- python_version/mqtt/settings.py
+++ python_version/mqtt/settings.py
@@ -15,3 +15,27 @@ TABLE_NAME = 'temp_test'
 MOCK_FILE = 'db.csv'
 
 HTTP_OK = 200
+
+# Heating
+HEATING = False
+THRESHOLD = 0.8
+
+CURRENT_TEMP = 22
+
+DAYS = ['Monday', 'Tuesday', "Wednesday", 'Thursday', "Friday", "Saturday", "Sunday"]
+
+HEATING_SETTINGS = {"Tuesday": [{'start_hour': 6,
+                         'start_min': 0,
+                         'end_hour': 23,
+                         'end_min': 30,
+                         'desired_temp': 24},
+
+                                {'start_hour': 23,
+                         'start_min': 31,
+                         'end_hour': 23,
+                         'end_min': 59,
+                         'desired_temp': 20
+                        }]
+                    }
+
+
